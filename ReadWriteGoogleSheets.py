ce920d8960eaf585c6e8be0f01c25c2118091995
Anastasia Polly <65036612+Dana1402@users.noreply.github.com>
2024-08-10 17:49:49 -0400

    Add files via upload

---
+++ ReadWriteGoogleSheets.py
@@ -0,0 +1,18 @@
+import gspread
+import csv
+from oauth2client.service_account import ServiceAccountCredentials
+
+scopes = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive']
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/anastasiya/PycharmProjects/Selenium/nimble-climber-432115-r6-f9c5fc5e77d1.json', scopes = scopes)
+
+file = gspread.authorize(creds)
+workbook = file.open("Indeed.com")
+sheet = workbook.sheet1
+
+csv_file = '/Users/anastasiya/PycharmProjects/Selenium/indeed_data.csv'
+with open(csv_file, "r") as f:
+    values = [r for r in csv.reader(f)]
+    sheet.update(values)
+
